fccd25daadb84d886294af4c2c321daae360e161
engfatmait <eng_fatma_it@yahoo.com>
2024-08-05 14:45:14 -0700

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Script that imports a Blueprint and runs Flask """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
